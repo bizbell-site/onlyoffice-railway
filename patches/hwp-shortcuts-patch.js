9b006ecc2b18ad1e0bf98a2610337dd90332825f
juhyun4910 <joomong4910@naver.com>
2026-02-04 14:03:20 +0900

    fix: CShortcuts 프로토타입 래핑 방식으로 단축키 등록 변경
    - AscCommon.AscShortcut 생성자 대신 CShortcuts.prototype.Get 래핑
    - 키 해시를 직접 계산하여 커스텀 맵에 저장
    - 사용 가능한 shortcut 타입 디버깅 출력 추가

--- patches/hwp-shortcuts-patch.js
+++ patches/hwp-shortcuts-patch.js
@@ -11,14 +11,51 @@
         // Asc 객체가 준비될 때까지 대기
         if (typeof Asc === 'undefined' ||
             typeof Asc.c_oAscDocumentShortcutType === 'undefined' ||
-            typeof Asc.c_oAscDefaultShortcuts === 'undefined' ||
-            typeof AscCommon === 'undefined') {
+            typeof AscCommon === 'undefined' ||
+            typeof AscCommon.CShortcuts === 'undefined') {
             setTimeout(initHwpShortcuts, 100);
             return;
         }
 
         var t = Asc.c_oAscDocumentShortcutType;
-        var d = Asc.c_oAscDefaultShortcuts;
+
+        // 키 해시 생성 함수 (OnlyOffice 내부 방식)
+        function makeKeyHash(keyCode, ctrl, shift, alt, cmd) {
+            var hash = keyCode;
+            if (ctrl) hash |= 0x0100;
+            if (shift) hash |= 0x0200;
+            if (alt) hash |= 0x0400;
+            if (cmd) hash |= 0x0800;
+            return hash;
+        }
+
+        // 단축키 추가 함수
+        function addShortcut(type, keyCode, ctrl, shift, alt, cmd) {
+            if (type === undefined || type === null) return false;
+            try {
+                var hash = makeKeyHash(keyCode, ctrl, shift, alt, cmd);
+                // CShortcuts 프로토타입에 접근하여 단축키 맵에 추가
+                if (AscCommon.CShortcuts.prototype && AscCommon.CShortcuts.prototype.Get) {
+                    // 기존 Get 메서드를 래핑
+                    var originalGet = AscCommon.CShortcuts.prototype.Get;
+                    if (!AscCommon.CShortcuts.prototype._bizbell_patched) {
+                        AscCommon.CShortcuts.prototype._bizbell_shortcuts = {};
+                        AscCommon.CShortcuts.prototype._bizbell_patched = true;
+                        AscCommon.CShortcuts.prototype.Get = function(nHash) {
+                            var custom = AscCommon.CShortcuts.prototype._bizbell_shortcuts[nHash];
+                            if (custom !== undefined) return custom;
+                            return originalGet.call(this, nHash);
+                        };
+                    }
+                    AscCommon.CShortcuts.prototype._bizbell_shortcuts[hash] = type;
+                    return true;
+                }
+                return false;
+            } catch(e) {
+                console.warn('[BizBell] Failed to add shortcut:', e);
+                return false;
+            }
+        }
 
         // 키코드 정의
         var KEY = {
@@ -29,117 +66,87 @@
             F7: 118, F8: 119, F9: 120, F10: 121, F11: 122, F12: 123,
             Enter: 13, Tab: 9, Space: 32, Escape: 27,
             BracketLeft: 219, BracketRight: 221,
-            Semicolon: 186, Quote: 222, Comma: 188, Period: 190,
-            Num1: 49, Num2: 50, Num3: 51, Num4: 52, Num5: 53,
-            Num6: 54, Num7: 55, Num8: 56, Num9: 57, Num0: 48
+            Semicolon: 186, Quote: 222, Comma: 188, Period: 190
         };
-
-        function addShortcut(type, keyCode, ctrl, shift, alt, cmd) {
-            if (type === undefined || type === null) return false;
-            try {
-                var shortcut = new AscCommon.AscShortcut(type, keyCode, ctrl, shift, alt, cmd);
-                if (!d[type]) {
-                    d[type] = shortcut;
-                } else if (Array.isArray(d[type])) {
-                    d[type].push(shortcut);
-                } else {
-                    d[type] = [d[type], shortcut];
-                }
-                return true;
-            } catch(e) {
-                console.warn('[BizBell] Failed to add shortcut:', e);
-                return false;
-            }
-        }
 
         var added = [];
 
-        // ========== 서식 관련 ==========
+        // ========== HWP 스타일 단축키 ==========
 
         // Alt+L: 글자 모양 (Font Dialog)
-        if (addShortcut(t.OpenFontDialog, KEY.L, false, false, true, false)) {
+        if (t.OpenFontDialog !== undefined && addShortcut(t.OpenFontDialog, KEY.L, false, false, true, false)) {
             added.push('Alt+L (글자 모양)');
         }
 
         // Alt+T: 문단 모양 (Paragraph Dialog)
-        if (addShortcut(t.OpenParagraphDialog, KEY.T, false, false, true, false)) {
+        if (t.OpenParagraphDialog !== undefined && addShortcut(t.OpenParagraphDialog, KEY.T, false, false, true, false)) {
             added.push('Alt+T (문단 모양)');
         }
 
         // Ctrl+]: 글자 크기 키우기
-        if (addShortcut(t.IncreaseFontSize, KEY.BracketRight, true, false, false, false)) {
+        if (t.IncreaseFontSize !== undefined && addShortcut(t.IncreaseFontSize, KEY.BracketRight, true, false, false, false)) {
             added.push('Ctrl+] (글자 크게)');
         }
 
         // Ctrl+[: 글자 크기 줄이기
-        if (addShortcut(t.DecreaseFontSize, KEY.BracketLeft, true, false, false, false)) {
+        if (t.DecreaseFontSize !== undefined && addShortcut(t.DecreaseFontSize, KEY.BracketLeft, true, false, false, false)) {
             added.push('Ctrl+[ (글자 작게)');
         }
 
-        // ========== 삽입 관련 ==========
-
         // Ctrl+F10: 특수문자 (Insert Symbol)
-        if (addShortcut(t.InsertSymbol, KEY.F10, true, false, false, false)) {
+        if (t.InsertSymbol !== undefined && addShortcut(t.InsertSymbol, KEY.F10, true, false, false, false)) {
             added.push('Ctrl+F10 (특수문자)');
         }
 
         // Ctrl+K: 하이퍼링크
-        if (addShortcut(t.InsertHyperlink, KEY.K, true, false, false, false)) {
+        if (t.InsertHyperlink !== undefined && addShortcut(t.InsertHyperlink, KEY.K, true, false, false, false)) {
             added.push('Ctrl+K (하이퍼링크)');
         }
 
         // Ctrl+Enter: 페이지 나누기
-        if (addShortcut(t.InsertPageBreak, KEY.Enter, true, false, false, false)) {
+        if (t.InsertPageBreak !== undefined && addShortcut(t.InsertPageBreak, KEY.Enter, true, false, false, false)) {
             added.push('Ctrl+Enter (페이지 나누기)');
         }
 
-        // ========== 편집/탐색 관련 ==========
-
         // Ctrl+H: 찾아 바꾸기
-        if (addShortcut(t.Replace, KEY.H, true, false, false, false)) {
+        if (t.Replace !== undefined && addShortcut(t.Replace, KEY.H, true, false, false, false)) {
             added.push('Ctrl+H (찾아 바꾸기)');
         }
 
         // F3: 다음 찾기
-        if (addShortcut(t.FindNext, KEY.F3, false, false, false, false)) {
+        if (t.FindNext !== undefined && addShortcut(t.FindNext, KEY.F3, false, false, false, false)) {
             added.push('F3 (다음 찾기)');
         }
 
-        // ========== 정렬 관련 ==========
-
         // Ctrl+Shift+L: 왼쪽 정렬
-        if (addShortcut(t.JustifyLeft, KEY.L, true, true, false, false)) {
+        if (t.JustifyLeft !== undefined && addShortcut(t.JustifyLeft, KEY.L, true, true, false, false)) {
             added.push('Ctrl+Shift+L (왼쪽 정렬)');
         }
 
         // Ctrl+Shift+R: 오른쪽 정렬
-        if (addShortcut(t.JustifyRight, KEY.R, true, true, false, false)) {
+        if (t.JustifyRight !== undefined && addShortcut(t.JustifyRight, KEY.R, true, true, false, false)) {
             added.push('Ctrl+Shift+R (오른쪽 정렬)');
         }
 
         // Ctrl+Shift+C: 가운데 정렬
-        if (addShortcut(t.JustifyCenter, KEY.C, true, true, false, false)) {
+        if (t.JustifyCenter !== undefined && addShortcut(t.JustifyCenter, KEY.C, true, true, false, false)) {
             added.push('Ctrl+Shift+C (가운데 정렬)');
         }
 
         // Ctrl+Shift+J: 양쪽 정렬
-        if (addShortcut(t.JustifyFull, KEY.J, true, true, false, false)) {
+        if (t.JustifyFull !== undefined && addShortcut(t.JustifyFull, KEY.J, true, true, false, false)) {
             added.push('Ctrl+Shift+J (양쪽 정렬)');
         }
 
-        // ========== 기타 ==========
-
-        // F4: 마지막 작업 반복
-        if (addShortcut(t.Repeat, KEY.F4, false, false, false, false)) {
-            added.push('F4 (반복)');
-        }
-
         // Ctrl+D: 글꼴 대화상자 (MS Word 호환)
-        if (addShortcut(t.OpenFontDialog, KEY.D, true, false, false, false)) {
+        if (t.OpenFontDialog !== undefined && addShortcut(t.OpenFontDialog, KEY.D, true, false, false, false)) {
             added.push('Ctrl+D (글꼴)');
         }
 
-        console.log('[BizBell] HWP shortcuts added:', added.length > 0 ? added.join(', ') : 'none');
+        console.log('[BizBell] HWP shortcuts initialized:', added.length > 0 ? added.join(', ') : 'none');
+
+        // 디버깅: 사용 가능한 shortcut 타입들 출력
+        console.log('[BizBell] Available shortcut types:', Object.keys(t).filter(k => t[k] !== undefined).join(', '));
     }
 
     // 즉시 실행 시도 + DOM 로드 후에도 시도
